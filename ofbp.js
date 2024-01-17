de6e5168ae529bef3c31b2420f2a5fe811cebcb1
Abdulsabur1 <abdulahmed3533@gmail.com>
2024-01-17 12:42:45 +0200

    The day i wrote my first javascritp with San Raph

---
+++ ofbp.js
@@ -0,0 +1,31 @@
+let name;
+
+name=window.prompt("input your name");
+
+
+if (name =="Abdul")
+
+{
+    window.alert("Abdul likes Fanta ");
+    console.log("Abdul likes Fanta ");
+}
+else if (name =="Christabel")
+{
+    window.alert("Christabel likes Water");
+    console.log("Christabel likes Fanta ");
+}
+else if (name =="David")
+{
+    window.alert("David likes Youghurt");
+    console.log("David likes Fanta ");
+}
+else if (name =="Raph")
+{
+    window.alert("Raph likes Amstel Malt");
+    console.log("Raph likes Fanta ");
+}
+else if (name =="Joel")
+{
+    window.alert("Joel likes coffee");
+    console.log("Joel likes fanta");
+}
